8742b423931c11a258a12e8669ba39220b066533
anton <anton@loofarna.se>
2020-03-01 20:45:22 +0100

    Move some stuff around

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 cp kandidat_nginx.conf /etc/nginx/sites-enabled/kandidat_nginx.conf
-source backend/measure/venv/bin/activate
+source backend/venv/bin/activate
 sh -c 'cd backend/measure && ./manage.py collectstatic --no-input'
 uwsgi --ini backend/measure.ini &
-sh -c 'cd frontend/measure && ng build --prod'
+# sh -c 'cd frontend/measure && npm run-script ng build -- --prod'
+
+mkdir -p /var/www/html/measure
 cp -r frontend/measure/dist/measure/* /var/www/html/measure
+
 /etc/init.d/nginx restart
 
