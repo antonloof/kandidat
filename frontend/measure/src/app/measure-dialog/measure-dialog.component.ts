c89f51bda950fa990ca3adc1c02fb8eab3aaf5c7
AlmaBlomback <63227038+AlmaBlomback@users.noreply.github.com>
2020-04-08 10:50:03 +0200

    Create measure-dialog.component.ts
    Mätningsdialog

---
+++ frontend/measure/src/app/measure-dialog/measure-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measure-dialog',
+  templateUrl: './measure-dialog.component.html',
+  styleUrls: ['./measure-dialog.component.css']
+})
+export class MeasureDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
