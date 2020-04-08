8b70050972f62305690060c799cb06e74a8997da
AlmaBlomback <63227038+AlmaBlomback@users.noreply.github.com>
2020-04-08 16:32:51 +0200

    Update measure-dialog.component.ts
    Uppdaterad version av mätningsdialogen. Såg att det fanns under classes "Measurement" och "CreateMeasurement", har inte hittat något sätt att integrera detta än, lite osäker på hur det ska fungera. Gjorde en egen klass tills vidare.

--- frontend/measure/src/app/measure-dialog/measure-dialog.component.ts
+++ frontend/measure/src/app/measure-dialog/measure-dialog.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../backend.service';
+import { mesure } from '../classes/mesure';
+import { MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+
+
 
 @Component({
   selector: 'app-measure-dialog',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./measure-dialog.component.css']
 })
 export class MeasureDialogComponent implements OnInit {
+  
+  speed = ['High Speed:Low Resulution', 'Medium speed: Medium Resulution', 'Low speed: High Resolution'];
 
-  constructor() { }
+  model = new mesure(1, 'name', 0 , this.speed[1]);
+
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+
+
+  constructor(
+    private backend: BackendService,
+    private dialogRef: MatDialogRef<MeasureDialogComponent>
+  ) {  }
 
   ngOnInit(): void {
   }
 
+  run(){
+    // got to home page.. and do measurement.
+    this.onSubmit()
+    this.dialogRef.close();
+  }
+  
+  
+  cancel(){
+     // GO HOME and clear. 
+     this.submitted = false;
+     this.dialogRef.close();
+  }
+  
+ 
 }
