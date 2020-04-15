d9c4f339f186d253c8fd5f84ee33ab20ba1b6d8c
AlmaBlomback <63227038+AlmaBlomback@users.noreply.github.com>
2020-04-15 09:15:44 +0200

    Resolved second wave of review comments

--- frontend/measure/src/app/measure-dialog/measure-dialog.component.ts
+++ frontend/measure/src/app/measure-dialog/measure-dialog.component.ts
@@ -1,99 +1,198 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from "@angular/material/dialog";
-import {FormControl, Validators, AbstractControl} from '@angular/forms';
-
+import { MatDialogRef } from '@angular/material/dialog';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+  AbstractControl,
+  FormGroupDirective,
+  NgForm,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { BackendService } from '../backend.service';
-import { CreateMeasurement } from '../classes/measurement';
 
 interface Speed {
   value: number;
   viewValue: string;
 }
-function currentValidator(control: AbstractControl): { [key: string]: boolean } | null {
+
+function currentValidator(
+  control: AbstractControl
+): { [key: string]: boolean } | null {
   if (control.value < 1e-9 || control.value > 3.3e-3) {
-    return { 'current': true };
+    return { current: true };
   }
   return null;
 }
 
+class CrossFieldErrorMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    return control.dirty && form.invalid;
+  }
+}
 
 @Component({
   selector: 'app-measure-dialog',
   templateUrl: './measure-dialog.component.html',
-  styleUrls: ['./measure-dialog.component.css']
+  styleUrls: ['./measure-dialog.component.css'],
 })
 export class MeasureDialogComponent implements OnInit {
   selectedValue: number;
-  error:string;
+  error: string;
+  form: FormGroup;
   nameControl = new FormControl('', [Validators.required]);
-  currentControl = new FormControl('', [Validators.required, currentValidator]);
+  currentControl = new FormControl(1e-6, [
+    Validators.required,
+    currentValidator,
+  ]);
+  connection1Control = new FormControl(null, [Validators.required]);
+  connection2Control = new FormControl(null, [Validators.required]);
+  connection3Control = new FormControl(null, [Validators.required]);
+  connection4Control = new FormControl(null, [Validators.required]);
+  descriptionControl = new FormControl('');
+  speedControl = new FormControl(10);
+  errorMatcher = new CrossFieldErrorMatcher();
+  submitted = false;
 
   speeds: Speed[] = [
-    {value: 2, viewValue: 'High Speed : Low Resolution'},
-    {value: 10, viewValue: 'Medium Speed : Medium Resolution'},
-    {value: 20, viewValue: 'Low Speed : High Resolution'}
+    { value: 20, viewValue: 'High Speed : Low Resolution' },
+    { value: 10, viewValue: 'Medium Speed : Medium Resolution' },
+    { value: 2, viewValue: 'Low Speed : High Resolution' },
   ];
-
-  measure: CreateMeasurement = {
-    connection_1: 1, 
-    connection_2: 2, 
-    connection_3: 3, 
-    connection_4: 4, 
-    current_limit: 1e-6, 
-    steps_per_measurement: 10,
-    name: 'Name', 
-    description:'Description'
-  };
 
   constructor(
     private backend: BackendService,
     private dialogRef: MatDialogRef<MeasureDialogComponent>,
-  ) {  }
-
-  submitted = false;
-  onSubmit() { this.submitted = true; }
-
-  ngOnInit(): void {
+    fb: FormBuilder
+  ) {
+    this.form = fb.group(
+      {
+        Con1: this.connection1Control,
+        Con2: this.connection2Control,
+        Con3: this.connection3Control,
+        Con4: this.connection4Control,
+        Speed: this.speedControl,
+        Current: this.currentControl,
+        Name: this.nameControl,
+        Description: this.descriptionControl,
+      },
+      {
+        validators: [
+          this.conMatchValidator1,
+          this.conMatchValidator2,
+          this.conMatchValidator3,
+          this.conMatchValidator4,
+        ],
+      }
+    );
   }
+  conMatchValidator1(form: FormGroup) {
+    if (
+      form.get('Con1').value === form.get('Con2').value ||
+      form.get('Con1').value === form.get('Con3').value ||
+      form.get('Con1').value === form.get('Con4').value
+    ) {
+      return { conMatch1: true };
+    } else {
+      return null;
+    }
+  }
+  conMatchValidator2(form: FormGroup) {
+    if (
+      form.get('Con2').value === form.get('Con1').value ||
+      form.get('Con2').value === form.get('Con3').value ||
+      form.get('Con2').value === form.get('Con4').value
+    ) {
+      return { conMatch2: true };
+    } else {
+      return null;
+    }
+  }
+  conMatchValidator3(form: FormGroup) {
+    if (
+      form.get('Con3').value === form.get('Con1').value ||
+      form.get('Con3').value === form.get('Con2').value ||
+      form.get('Con3').value === form.get('Con4').value
+    ) {
+      return { conMatch3: true };
+    } else {
+      return null;
+    }
+  }
+  conMatchValidator4(form: FormGroup) {
+    if (
+      form.get('Con4').value === form.get('Con1').value ||
+      form.get('Con4').value === form.get('Con3').value ||
+      form.get('Con4').value === form.get('Con2').value
+    ) {
+      return { conMatch4: true };
+    } else {
+      return null;
+    }
+  }
+  ngOnInit(): void {}
 
   getNameErrorMessage() {
     if (this.nameControl.hasError('required')) {
       return 'You must name your measurement!';
     }
   }
-  getCurrentErrorMessage(){
-    if (this.currentControl.hasError('required')){
-      return 'You must giva a target current!'
+  getCurrentErrorMessage() {
+    if (this.currentControl.hasError('required')) {
+      return 'You must giva a target current!';
+    } else if (this.currentControl.hasError('current')) {
+      return 'Target current must be between 1nA and 3.3mA!';
     }
-    else if (this.currentControl.hasError('current')){
-      return 'Target current must be between 1nA and 3.3mA!'
+  }
+  getConnection1ErrorMessage() {
+    if (this.connection1Control.hasError('required')) {
+      return 'You must fill in the correct connections!';
+    }
+  }
+  getConnection2ErrorMessage() {
+    if (this.connection2Control.hasError('required')) {
+      return 'You must fill in the correct connections!';
+    }
+  }
+  getConnection3ErrorMessage() {
+    if (this.connection3Control.hasError('required')) {
+      return 'You must fill in the correct connections!';
+    }
+  }
+  getConnection4ErrorMessage() {
+    if (this.connection4Control.hasError('required')) {
+      return 'You must fill in the correct connections!';
     }
   }
 
-  run(){
-    if(this.nameControl.hasError || this.currentControl.hasError){ }
-    else{
-      this.onSubmit()
-      this.dialogRef.close()
-      this.backend
-      .create_measurement({
-        connection_1: this.measure.connection_1,
-        connection_2: this.measure.connection_2,
-        connection_3: this.measure.connection_3,
-        connection_4: this.measure.connection_4,
-        current_limit: this.measure.current_limit,
-        steps_per_measurement: this.selectedValue,
-        name: this.measure.name,
-        description: this.measure.description,
-      });  
+  startMeasurement() {
+    if (
+      !this.nameControl.hasError('required') &&
+      !this.currentControl.hasError('required') &&
+      !this.currentControl.hasError('current') &&
+      !this.form.hasError('conMatch1') &&
+      !this.form.hasError('conMatch2') &&
+      !this.form.hasError('conMatch3') &&
+      !this.form.hasError('conMatch4')
+    ) {
+      this.dialogRef.close();
+      this.backend.create_measurement({
+        connection_1: this.connection1Control.value,
+        connection_2: this.connection2Control.value,
+        connection_3: this.connection3Control.value,
+        connection_4: this.connection4Control.value,
+        current_limit: this.currentControl.value,
+        steps_per_measurement: this.speedControl.value,
+        name: this.nameControl.value,
+        description: this.descriptionControl.value,
+      }).subscribe;
     }
   }
-  
-  cancel(){
-     this.submitted = false;
-     this.dialogRef.close();
+  cancel() {
+    this.dialogRef.close();
   }
-  
 }
-
-
